0f8fe1b1d62f4ccdb2b56cab5a8528bc09b9d7a2
Amaya-2003 <caluchogamer@gmail.com>
2025-02-19 22:28:33 -0600

    Categoria class

---
+++ Repaso/Modelos/categoria.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Repaso.Modelos
+{
+    public class categoria
+    {
+        [Key]
+        public int categoriaId { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+    }
+}
