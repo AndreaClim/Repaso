12a31ae377085b78c506773916b2b6414c17ba1e
miche <miche@DESKTOP-EEADCHD>
2025-02-19 22:35:36 -0600

    xxx

---
+++ Repaso/Modelos/BibliotecaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Repaso.Modelos
+{
+    public class BibliotecaContext : DbContext
+    {
+
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<libro> libros { get; set; }
+        public DbSet<autor> autores { get; set; } 
+        public DbSet<categoria> categorias { get; set; }
+    }
+}
